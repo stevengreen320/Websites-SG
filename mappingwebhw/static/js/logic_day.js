014fe4bd827cbe4b35db0425a16380cac294dbe9
stevengreen320 <85272745+stevengreen320@users.noreply.github.com>
2021-10-27 18:28:09 -0500

    Update logic_day.js

--- mappingwebhw/static/js/logic_day.js
+++ mappingwebhw/static/js/logic_day.js
@@ -7,7 +7,7 @@ var timeframe = "all_day.geojson";
 var url = baseURL + timeframe;
 
 // geojson
-var tectonic_plates_url = "../static/data";
+var tectonic_plates_url = "..mappingwebhw/static/data.json";
 $(document).ready(function() {
     // AJAX
     $.ajax({
